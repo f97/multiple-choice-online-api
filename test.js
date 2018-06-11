62be9a7bee060657550a9db3d08d70f008801abb
Huỳnh Đức Khoản <huynhduckhoan@gmail.com>
2018-06-11 14:52:20 +0700

    Update test

---
+++ test.js
@@ -0,0 +1,31 @@
+'use strict';
+var https = require('https');
+
+var options = {
+    host: 'multiple-choice-online-api.herokuapp.com',
+    path: '/user',
+    headers: {'User-Agent': 'request'}
+};
+
+https.get(options, function (res) {
+    var json = '';
+    res.on('data', function (chunk) {
+        json += chunk;
+    });
+    res.on('end', function () {
+        if (res.statusCode === 200) {
+            try {
+                var data = JSON.parse(json);
+                // data is available here:
+                console.log(data);
+                console.log(data.html_url);
+            } catch (e) {
+                console.log('Error parsing JSON!');
+            }
+        } else {
+            console.log('Status:', res.statusCode);
+        }
+    });
+}).on('error', function (err) {
+      console.log('Error:', err);
+});
